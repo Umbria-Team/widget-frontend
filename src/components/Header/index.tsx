b8202ef86e02045ee00a5e70999261dc003d8349
Aaron Goulden <aaron@codabit.ltd>
2021-11-30 10:57:36 +0100

    Fixed discord link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -48,7 +48,9 @@ function AppBar(): JSX.Element {
                     <div className="w-auto flex items-center rounded bg-dark-900 hover:bg-dark-800 p-0.5 whitespace-nowrap text-sm font-bold cursor-pointer select-none pointer-events-auto">
                       <div className="px-3 py-2 text-primary text-bold">
                         <span>
-                          <a href="https://discord.umbria.network">Discord / Support</a>
+                          <a href="https://discord.umbria.network" target="_blank">
+                            Discord / Support
+                          </a>
                         </span>
                       </div>
                     </div>
